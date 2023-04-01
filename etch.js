1d67d76df6ba7cecc55f253145ab297fafd9ab64
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2023-04-01 17:54:16 +0530

    made dimensions adjustable

--- etch.js
+++ etch.js
@@ -2,20 +2,16 @@ const gridContainer = document.querySelector("#grid-container");
 createGrid(16);
 
 function createGrid(x){
-  for(i=0;i<16;i++){
-    for(j=0;j<16;j++){
+  for(i=0;i<x;i++){
+    for(j=0;j<x;j++){
       const divs =document.createElement('div');
       gridContainer.appendChild(divs);
       divs.classList.add("cell");
       console.log(divs);
-            let height=960/x;
-            let width =960/x;
-            divs.style.width=`${height}px`;
-            divs.style.height=`${width}px`;
+            let height=x*60+x+5;
+            let width =x*60+x+5;
+            gridContainer.style.width=`${height}px`;
+            gridContainer.style.height=`${width}px`;
     }
   }
-            //let height=972/x-1;
-            //let width =972/x-1;
-            //divs.style.width=`${height}px`;
-            //divs.style.height=`${width}px`;
 }
