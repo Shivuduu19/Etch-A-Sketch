ca787e1b6e60927466e421fbfe59cec1ba175f03
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2023-04-01 18:35:49 +0530

    added eventlistener

--- etch.js
+++ etch.js
@@ -3,15 +3,19 @@ createGrid(16);
 
 function createGrid(x){
   for(i=0;i<x;i++){
-    for(j=0;j<x;j++){
-      const divs =document.createElement('div');
-      gridContainer.appendChild(divs);
-      divs.classList.add("cell");
-      console.log(divs);
-            let height=x*60+x+5;
-            let width =x*60+x+5;
-            gridContainer.style.width=`${height}px`;
-            gridContainer.style.height=`${width}px`;
+    for(j=0;j<x;j++)
+    {
+     const divs =document.createElement('div');
+     gridContainer.appendChild(divs);
+     divs.classList.add("cell");
+     let height=x*60+x+5;
+     let width =x*60+x+5;
+     gridContainer.style.width=`${height}px`;
+     gridContainer.style.height=`${width}px`;
+     divs.addEventListener("mouseover",function(e)
+     {
+      e.target.classList.add("change");
+     });
     }
   }
 }
